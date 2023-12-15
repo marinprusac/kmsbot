bdd20e092c219953b57c12e2a58559515ed32096
Marin Prusac <prusac.marin@gmail.com>
2023-12-15 04:19:26 +0100

    some update

--- datahandler.py
+++ datahandler.py
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import json
-
-
-class DataHandler:
-	data: dict = {}
-
-	@staticmethod
-	def load():
-		with open('./data.json') as file:
-			DataHandler.data = json.loads(file.read())
-
-	@staticmethod
-	def save():
-		with open('./data-backup.json', 'w') as backup, open('./data.json', 'r') as file:
-			backup.write(file.read())
-		try:
-			with open('./data.json', 'w') as file:
-				file.write(json.dumps(DataHandler.data, indent=4))
-		except BaseException as err:
-			with open('./data.json', 'w') as file, open('./data-backup.json', 'r') as backup:
-				file.write(backup.read())
-			raise err
-
-	@staticmethod
-	def get(field: str):
-		if len(DataHandler.data) == 0:
-			DataHandler.load()
-		return DataHandler.data[field]
-
-	@staticmethod
-	def set(field: str, data):
-		DataHandler.data[field] = data
-		DataHandler.save()
-=======
 import jsonpickle
 
 
@@ -139,4 +104,3 @@ class AllData:
 			with open(f'./data/{self.guild_id}.json', 'w') as file, open(f'./data/{self.guild_id}-backup.json', 'r') as backup:
 				file.write(backup.read())
 			raise err
->>>>>>> 7ae79f15b070aca33c0da775006268eb945ae0d4
