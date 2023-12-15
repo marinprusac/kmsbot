6a7d289a0de8e2b5800b0b852c116eb17e43d0a7
Marin Prusac <prusac.marin@gmail.com>
2023-12-15 04:54:29 +0100

    added token as a required parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import discord
 from discord.ext import commands
@@ -36,7 +37,7 @@ async def on_ready():
 
 
 def main():
-    bot.run('MTE3OTE1MzU3Njk0MjExMjg0OQ.GXQlhX.GV_cNl_YZevhkeZEg7hJgtxmjhIaM2jU0ot1vQ')
+    bot.run(sys.argv[1])
 
 
 if __name__ == '__main__':
